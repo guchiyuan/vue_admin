2a06833586d85605987bef2e4ba05a66e7844f87
guchiyuan <c11044gcy@163.com>
2018-05-25 15:39:26 +0800

    init project

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    home
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
+
